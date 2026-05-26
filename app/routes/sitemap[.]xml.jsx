7edc7503d926fbf6dc714f11e7b4befa0198a4ac
Sourav <sourav.malakar@webskitters.com>
2026-05-26 17:48:40 +0530

    add sitemap resource route

---
+++ app/routes/sitemap[.]xml.jsx
@@ -0,0 +1,17 @@
+export const loader = () => {
+  const baseUrl = "https://song-pettus-customiser.vercel.app";
+
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${baseUrl}/</loc>
+  </url>
+</urlset>`;
+
+  return new Response(body, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "public, max-age=3600",
+    },
+  });
+};
