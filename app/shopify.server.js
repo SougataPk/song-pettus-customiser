8daa60a0030beee4d7bde3125a5fb30e38d590e2
Sourav <sourav.malakar@webskitters.com>
2026-05-26 19:22:39 +0530

    Cloudflare tunnel issued fixed-v2

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -7,12 +7,15 @@ import {
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
 import prisma from "./db.server";
 
+const appUrl =
+  process.env.SHOPIFY_APP_URL || "https://song-pettus-customiser.vercel.app";
+
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
   apiVersion: ApiVersion.October25,
   scopes: process.env.SCOPES?.split(","),
-  appUrl: process.env.SHOPIFY_APP_URL || "",
+  appUrl,
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
